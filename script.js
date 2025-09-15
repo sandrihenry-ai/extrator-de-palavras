30fc203b1c11c6ec89e1bfab2724fa7d6ca0371a
sandrihenry-ai <sandri.henry@escola.pr.gov.br>
2025-09-15 10:54:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavraschave");
-botaoMostraPalavras.addEventListemer("click" , MostraPalavrasChave);
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+botaoMostraPalavras.addEventListener("click" , MostraPalavrasChave);
 function MostraPalavrasChave(){
-alert(" fui clicado!");
+const texto = document.querySelector("#entrada-de-texto").value;
+const campoResultado = document.querySelector("#resultado-palavrachave");
+campoResultado.textContent = texto;
+const palavraChave = processaTexto(texto);
+campoResultado.textContent = palavras.join(" , ");
+}
+function processaTexto(texto){
+let palavras = texto.split(/\P{L}+/u);
+return palavras;
 }
