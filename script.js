df7c85dded266d330845260dd41b9e7172a97825
sandrihenry-ai <sandri.henry@escola.pr.gov.br>
2025-09-22 10:46:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ function MostraPalavrasChave(){
 const texto = document.querySelector("#entrada-de-texto").value;
 const campoResultado = document.querySelector("#resultado-palavrachave");
 campoResultado.textContent = texto;
-const palavraChave = processaTexto(texto);
+const palavrasChave = processaTexto(texto);
 campoResultado.textContent = palavras.join(" , ");
 }
 function processaTexto(texto){
-let palavras = texto.split(/\P{L}+/u);
-return palavras;
+    let palavras = texto.split(/\P{L}+/u);
+
+    let frequencias = {};
+    for (let i of palavras){
+        frequencias [i] = 0;
+
+        for(let j of palavras){
+            if ( i == j){
+                frequencias[i] ++;
+            }
+        }
+    }
+    console.log(frequencias);
+    return palavras;
 }
